ee0548eb00d2ccaab61256f49c3aea1d81620439
Tcohneyn <chenminami@outlook.com>
2025-09-05 23:56:31 +0800

    第四章 属性

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,49 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+    InitHealth(100.f);
+    InitMaxHealth(100.f);
+    InitMana(50.f);
+    InitMaxMana(50.f);
+}
+// 网络同步属性时需要重写这个函数
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    // 使用 DOREPLIFETIME_CONDITION_NOTIFY 宏注册属性的同步规则
+    // 参数依次是：类名、属性名、同步条件、是否始终触发 OnRep
+    // REPNOTIFY_Always 表示即使值没变也会触发 OnRep_XXX 
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+// ---------------------- OnRep 回调 ----------------------
+// 当属性在客户端被复制（replicate）时会调用这些函数
+// OldXXX 参数表示同步前的旧值
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+    // GAMEPLAYATTRIBUTE_REPNOTIFY 宏：
+    // 通知 AbilitySystemComponent 该属性发生变化，触发相关回调（比如 UI 更新）
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+}
+
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Mana,OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxMana,OldMaxMana);
+}
---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,49 @@
+// Tcohneyn All Rights Reserved
+
+
+#include "Actor/AuraEffectActor.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    SetRootComponent(Mesh);
+
+    Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+    Sphere->SetupAttachment(GetRootComponent());
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    //TODO:之后要改成应用一个 Gameplay Effect。目前先用 const_cast 临时凑合一下！
+    if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+    {
+        const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+
+        UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+        MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f);
+        Destroy();
+    }
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex)
+{
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+    Sphere->OnComponentBeginOverlap.AddDynamic(this,&ThisClass::OnOverlap);
+    Sphere->OnComponentEndOverlap.AddDynamic(this,&ThisClass::EndOverlap);
+}
+
+
