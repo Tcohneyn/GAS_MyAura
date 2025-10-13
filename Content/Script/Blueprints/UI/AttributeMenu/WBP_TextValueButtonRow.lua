0ceac8b9106c7042c8789ecbaa1723bfd8d1eeca
Tcohneyn <chenminami@outlook.com>
2025-10-13 17:54:42 +0800

    修复派生属性的显示问题

--- Content/Script/Blueprints/UI/AttributeMenu/WBP_TextValueButtonRow.lua
+++ Content/Script/Blueprints/UI/AttributeMenu/WBP_TextValueButtonRow.lua
@@ -9,16 +9,18 @@
 ---@type WBP_TextValueButtonRow_C
 local M = UnLua.Class("Blueprints.UI.AttributeMenu.WBP_TextValueRow")
 
-function M:Construct()
-   local AMController = UE.UAuraAbilitySystemLibrary.GetAttributeMenuWidgetController(self)
-   AMController.AttributeInfoDelegate:Add(self, M.OnAttributeInfoReceived)
-end
+-- function M:Construct()
+--   self.Super.Construct(self)
+--   --  local AMController = UE.UAuraAbilitySystemLibrary.GetAttributeMenuWidgetController(self)
+--   --  AMController.AttributeInfoDelegate:Add(self, M.OnAttributeInfoReceived)
+-- end
 
-function M:OnAttributeInfoReceived(Info)
-  --print(UE.UBlueprintGameplayTagLibrary.GetTagName(self.AttributeTag))
-  if UE.UBlueprintGameplayTagLibrary.MatchesTag(Info.AttributeTag, self.AttributeTag, true) then
-   self:SetLabelText(Info.AttributeName)
-   self:SetNumericalValueInt(UE.UKismetMathLibrary.FTrunc(Info.AttributeValue))
-  end
-end 
+-- function M:OnAttributeInfoReceived(Info)
+--   --self.Super.OnAttributeInfoReceived(self)
+--   -- if UE.UBlueprintGameplayTagLibrary.MatchesTag(Info.AttributeTag, self.AttributeTag, true) then
+--   --  print(UE.UBlueprintGameplayTagLibrary.GetTagName(self.AttributeTag))
+--   --  self:SetLabelText(Info.AttributeName)
+--   --  self:SetNumericalValueInt(UE.UKismetMathLibrary.FTrunc(Info.AttributeValue))
+--   -- end
+-- end 
 return M
--- Content/Script/Blueprints/UI/AttributeMenu/WBP_TextValueRow.lua
+++ Content/Script/Blueprints/UI/AttributeMenu/WBP_TextValueRow.lua
@@ -16,4 +16,17 @@ end
 function M:SetNumericalValueInt(NewValue)
     self.WBP_FramedValue.TextBlock_Value:SetText(UE.UKismetTextLibrary.Conv_IntToText(NewValue))
 end
+
+function M:Construct()
+   local AMController = UE.UAuraAbilitySystemLibrary.GetAttributeMenuWidgetController(self)
+   AMController.AttributeInfoDelegate:Add(self, M.OnAttributeInfoReceived)
+end
+
+function M:OnAttributeInfoReceived(Info)
+  if UE.UBlueprintGameplayTagLibrary.MatchesTag(Info.AttributeTag, self.AttributeTag, true) then
+   --print(UE.UBlueprintGameplayTagLibrary.GetTagName(self.AttributeTag))
+   self:SetLabelText(Info.AttributeName)
+   self:SetNumericalValueInt(UE.UKismetMathLibrary.FTrunc(Info.AttributeValue))
+  end
+end 
 return M
