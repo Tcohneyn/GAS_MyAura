62d0bb9386f603c7a62520b1e2ac18a79893a45e
Tcohneyn <chenminami@outlook.com>
2026-01-19 06:06:12 +0800

    第二十章 关卡调整

--- Content/Script/Blueprints/Character/Aura/BP_AuraCharacter.lua
+++ Content/Script/Blueprints/Character/Aura/BP_AuraCharacter.lua
@@ -9,6 +9,25 @@
 ---@type BP_AuraCharacter_C
 local M = UnLua.Class()
 
+function M:ReceiveBeginPlay()
+    -- 给Sphere绑定Overlap事件
+    self.Box.OnComponentBeginOverlap:Add(self, M.OnBoxBeginOverlap)
+    self.Box.OnComponentEndOverlap:Add(self, M.OnBoxEndOverlap)
+end
+
+function M:OnBoxBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult)
+    local value = UE.UKismetSystemLibrary.DoesImplementInterface(OtherActor, self.Interface)
+    if value then
+        OtherActor:FadeOut()
+    end
+end
+
+function M:OnBoxEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult)
+       local value = UE.UKismetSystemLibrary.DoesImplementInterface(OtherActor, self.Interface)
+    if value then
+        OtherActor:FadeIn()
+    end
+end
 function M:UpdateFacingTarget(Target)
    self.MotionWarping:AddOrUpdateWarpTargetFromLocation("FaceTarget", Target)
 end
